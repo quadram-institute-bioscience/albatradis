9c5f7a4ad51b3a3bdcf7ce55fca93ed347c2c2a4
Andrew Page <andrew.page@quadram.ac.uk>
2018-07-04 10:33:09 +0100

    remove common name from heat map

--- tradistron/HeatMap.py
+++ tradistron/HeatMap.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
+import os
 
 class HeatMap:
 	def __init__(self, reports_to_gene_logfc, gene_names, outputfile):
@@ -11,18 +12,32 @@ class HeatMap:
 		
 	def clean_filenames(self):
 		cleaned_names = []
+		
+		filenames = self.reports_to_gene_logfc.keys()
+		common_prefix = os.path.commonpath(filenames)
+		
 		for n in self.reports_to_gene_logfc.keys():
-			cleaned_names.append(n.replace("/gene_report.csv",""))
+			updated_name = n.replace("/gene_report.csv","")
+			updated_name = updated_name.replace(".csv","")
+			updated_name = updated_name.replace("_"," ")
+			if len(common_prefix) > 1:
+				updated_name = updated_name.replace(common_prefix,"").strip("/")
+			cleaned_names.append(updated_name)
+			
 		return cleaned_names
 
 	def create_heat_map(self):
 		int_values = [[int(y) for y in x] for x in self.reports_to_gene_logfc.values()]
 		
-		a4_dims = (8.27, 11.7)
-		fig, ax = plt.subplots(figsize=a4_dims)
+		dims = (16.54, 23.4)
+		fig, ax = plt.subplots(figsize = dims)
 		data = pd.DataFrame(int_values, columns = self.gene_names, index=self.clean_filenames())
 
 		heatmap = sns.heatmap(data.T,ax = ax, cmap="RdBu_r", center=0, yticklabels= False)
+		
+		ax.set_xticklabels(ax.get_xticklabels(), rotation=40, ha="right")
+		plt.tight_layout()
+		
 		plt.savefig(self.outputfile , dpi=100)
 		plt.close()
-		
+		return True
---
+++ tradistron/tests/HeatMap_test.py
@@ -0,0 +1,35 @@
+import unittest
+import os
+import logging
+import filecmp
+from tradistron.HeatMap import HeatMap
+
+class ErrorReadingFile (Exception): pass
+class InvalidFileFormat (Exception): pass
+
+test_modules_dir = os.path.dirname(os.path.realpath(__file__))
+data_dir = os.path.join(test_modules_dir, 'data','heatmap')
+
+class TestNormalisePlots(unittest.TestCase):
+
+	def test_same_path(self):
+		output_file = os.path.join(data_dir,'heatmap.png')
+		reports_to_logfc = {'/path/Antibiotic_1.csv': [1,5,0], '/path/Antibiotic_2': [0,10,-10], '/path/someotherAntibiotic_0.5': [0,1,0]}
+		h = HeatMap(reports_to_logfc, ['geneA','geneB', 'geneC'], output_file)
+
+
+		self.assertEqual(['Antibiotic 1', 'Antibiotic 2', 'someotherAntibiotic 0.5'], h.clean_filenames())
+		self.assertTrue(h.create_heat_map())
+		self.assertTrue(os.path.exists(output_file))
+		os.remove(output_file)
+		
+	def test_no_common_path(self):
+		output_file = os.path.join(data_dir,'heatmap.png')
+		reports_to_logfc = {'/path123/Antibiotic_1.csv': [1,5,0], '/path456/Antibiotic_2': [0,10,-10], '/path/someotherAntibiotic_0.5': [0,1,0]}
+		h = HeatMap(reports_to_logfc, ['geneA','geneB', 'geneC'], output_file)
+
+		self.assertEqual(['/path123/Antibiotic 1', '/path456/Antibiotic 2', '/path/someotherAntibiotic 0.5'], h.clean_filenames())
+		self.assertTrue(h.create_heat_map())
+		self.assertTrue(os.path.exists(output_file))
+		os.remove(output_file)
+		
